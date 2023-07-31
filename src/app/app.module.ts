43fac63dfcf188036d3109ef81002ecffd9c5efa
Pedro Lambarri <plambarri@gmail.com>
2023-07-31 18:58:18 +0200

    fin de seccion 4 de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CounterModule } from './counter/components/counter.module';
 import { AppComponent } from './app.component';
+import { HeroModule } from './heroes/hero/heros.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
