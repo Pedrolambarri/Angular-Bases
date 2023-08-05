d095a231f280cd610d8070d1f78a44cf6e03c322
Pedro Lambarri <plambarri@gmail.com>
2023-08-05 10:50:43 +0200

    deploying in github pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/components/counter.module';
 import { AppComponent } from './app.component';
 import { HeroModule } from './heroes/hero/heros.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroModule],
+  imports: [BrowserModule, CounterModule, HeroModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
